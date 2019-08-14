8e279161238d30d9c5cf5d799e717235c7838be3
libowen02 <libowen02@meicai.cn>
2019-08-14 19:41:51 +0800

    fetch tags

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const exec = require('child_process').execSync
 const moment = require('moment')
 const tag_stamp = moment(new Date()).format('YYMMDD');
 const cmd_tag = "git tag"
+const cmd_fetch_tag = 'git fetch --tags'
 let tagArr = []
 let thisTag = null
 
 function resTag(){
+    console.log(chalk.green(`fetching...`))
+    exec(cmd_fetch_tag, {encoding:'utf8'})
     return exec(cmd_tag, {encoding:'utf8'})
 }
 function createAndPushTag(version_num){
@@ -16,7 +19,7 @@ function createAndPushTag(version_num){
     let cmd_push_tag = `git push origin v_${tag_stamp}${version_num}`
     exec(cmd_create_tag, {encoding:'utf8'})
     exec(cmd_push_tag, {encoding:'utf8'})
-    console.log(chalk.green(`v_${tag_stamp}${version_num}`))
+    console.log(chalk.green(`Tag: v_${tag_stamp}${version_num} success`))
 }
 let allTags = resTag()
 const arr = allTags.split('\n')
